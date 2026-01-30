275fd6e2f3e79730461c70978ffd8fbb0d56588e
jpgcano <juan-0395@hotmail.com>
2026-01-30 10:10:59 -0500

    creando login

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import { router } from "./src/";
+import { router } from "./src/router/router";
 
 
 
-
+window.addEventListener('load',router);
 window.addEventListener('hashchange',router);
+
+
+
+console.log("app.js existoso")
--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,27 @@
-export function router (){
-    
+const routes = {
+    "login": "./src/view/login.html"
+
 }
+
+export async function  router(){
+    const hash = location.hash || "#/login";
+    const path = hash.split('/')[1] || "login";
+    
+    const route = routes[path];
+
+    try {
+        const response = await fetch(route);
+        if (!response.ok) throw new Error("Página no encontrada");
+        const html = await response.text(); 
+        const render = document.querySelector("#app");
+        render.innerHTML = html;
+    } catch (error) {
+        console.error("Error en el router:", error);
+        document.querySelector("#app").innerHTML = "<h1>404 - No encontrado</h1>";
+    }
+
+    console.log("router.js exitoso");
+}
+
+
+
